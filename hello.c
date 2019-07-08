f822621f815b574facbd7f0e2bd62a81c69e4597
Bocquet <allan1243@outlook.fr>
2019-07-08 11:02:47 +0200

    first commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//agrc: argument count
+//agrv: argument vector
+int main(int argc, char* argv[]) {
+    //affichage d'un message
+    printf("Hello World!\n");
+    //il n'y a pas eu d'erreur
+    return 0;
+}
